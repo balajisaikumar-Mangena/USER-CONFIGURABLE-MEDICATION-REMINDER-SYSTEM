1cb0790680c0bfde9fad88f64540c9c9fc7813e4
Balaji Sai Kumar Mangena <balajisaimangena@gmail.com>
2026-06-01 14:16:19 +0530

    Remove all files except README

--- KPM.h
+++
@@ -1,6 +0,0 @@
-/*====================================================================
- *                  KEYPAD FUNCTION DECLARATIONS
- *====================================================================*/
-
-void init_KPM(void);              // Initialize keypad pins
-unsigned char Keypad_Scan(void);  // Scan keypad and return pressed key
--- delay.h
+++
@@ -1,7 +0,0 @@
-/*====================================================================
- *                  DELAY FUNCTION DECLARATIONS
- *====================================================================*/
-
-void delay_s(unsigned int d_s);    // Delay in seconds
-void delay_ms(unsigned int d_ms);  // Delay in milliseconds
-void delay_us(unsigned int d_us);  // Delay in microseconds
